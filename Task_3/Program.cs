bfa098cfc93dba161d5990be1dcf73907d2e6765
«Kristina» <kristinabol1996@gmail.com>
2022-09-04 04:31:25 +0800

    3 задача

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigits(int number)
+{
+    if (number == 0)
+        return 0;
+    return (number % 10) + SumDigits(number / 10);
+}
+
+Console.Write(SumDigits(9));
